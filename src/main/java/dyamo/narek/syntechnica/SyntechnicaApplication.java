fb88a1ae0017c2eabbebdad32f9b493c32c214d8
dyamo <rikkanavo@gmail.com>
2023-07-16 22:10:41 +0400

    Added provision and persistence of a KeyStore with a self-signed certificate.

--- src/main/java/dyamo/narek/syntechnica/SyntechnicaApplication.java
+++ src/main/java/dyamo/narek/syntechnica/SyntechnicaApplication.java
@@ -2,8 +2,10 @@ package dyamo.narek.syntechnica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SyntechnicaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SyntechnicaApplication.class, args);
