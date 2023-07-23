44f4a8c4140a1a3288c798785cbf33353598a4cd
dyamo <rikkanavo@gmail.com>
2023-07-23 15:54:29 +0400

    Added DefaultExceptionHandlerAspect tests.

---
+++ src/test/java/dyamo/narek/syntechnica/global/AopProxyBuilder.java
@@ -0,0 +1,25 @@
+package dyamo.narek.syntechnica.global;
+
+import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
+import org.springframework.aop.framework.AopProxy;
+import org.springframework.aop.framework.DefaultAopProxyFactory;
+
+public class AopProxyBuilder {
+
+	private AopProxyBuilder() {}
+
+
+	@SuppressWarnings("unchecked")
+	public static <T> T aopProxy(T target, Class<?>... aspectClasses) {
+		AspectJProxyFactory aspectJProxyFactory = new AspectJProxyFactory(target);
+		for (Class<?> aspectClass : aspectClasses) {
+			aspectJProxyFactory.addAspect(aspectClass);
+		}
+
+		DefaultAopProxyFactory proxyFactory = new DefaultAopProxyFactory();
+		AopProxy aopProxy = proxyFactory.createAopProxy(aspectJProxyFactory);
+
+		return (T) aopProxy.getProxy();
+	}
+
+}
